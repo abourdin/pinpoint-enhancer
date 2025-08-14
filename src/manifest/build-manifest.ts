df43296f3a4d9dab25e20fd3ae768936891605aa
Alex Bourdin <alex.bourdin@overleaf.com>
2025-08-14 16:14:10 +0200

    Remove default host permission from manifest

--- src/manifest/build-manifest.ts
+++ src/manifest/build-manifest.ts
@@ -1,13 +1,13 @@
-import { ManifestConfig, ManifestContentScript } from '@types';
+import { ManifestConfig, ManifestContentScript } from '@types'
 
-import { buildManifestV2 } from "./build-manifest-v2";
-import { buildManifestV3 } from "./build-manifest-v3";
-import { contentScripts } from '../content-scripts';
+import { buildManifestV2 } from './build-manifest-v2'
+import { buildManifestV3 } from './build-manifest-v3'
+import { contentScripts } from '../content-scripts'
 
 export function buildManifest(version: number, config: ManifestConfig) {
-  const hostPermissions = ['https://example.com/*'];
+  const hostPermissions: string[] = []
   if (process.env.NODE_ENV === 'development') {
-    hostPermissions.push('http://localhost:3000/*');
+    hostPermissions.push('http://localhost:3000/*')
   }
 
   const scripts = Object.entries(contentScripts)
@@ -15,14 +15,14 @@ export function buildManifest(version: number, config: ManifestConfig) {
       ...getConfig({ hostPermissions }),
       js: [config.content[name].js],
       css: [config.content[name].css],
-    }));
+    }))
 
   switch (version) {
     case 2: {
-      return buildManifestV2(config, hostPermissions, scripts);
+      return buildManifestV2(config, hostPermissions, scripts)
     }
     case 3: {
-      return buildManifestV3(config, hostPermissions, scripts);
+      return buildManifestV3(config, hostPermissions, scripts)
     }
   }
 }
